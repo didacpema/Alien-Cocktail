de363b09445d85298e3c48b7f2bc56bd242e961e
Carla Puga <84739899+cpugaper@users.noreply.github.com>
2025-05-08 21:30:40 +0200

    SceneManager (1/2)

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,18 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static SceneManager Instance { get; private set; }
+
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadMainMenu()
     {
-        
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadDemoScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("DemoScene");
+
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.StartNewShift();
+        }
+    }
+
+    public void LoadResultsScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("ResultsScene");
+
+        //if (GameManager.Instance != null && GameManager.Instance.resultManager != null)
+        //{
+        //    GameManager.Instance.resultManager.CalculateFinalResults(GameManager.Instance.GetCurrentScore());
+        //}
+    }
+
+    public void OnReturnToMainMenu()
+    {
+        LoadMainMenu();
+    }
+
+    public void OnPlayAgain()
+    {
+        LoadDemoScene();
+    }
+
+    public void OnShiftCompleted()
+    {
+        LoadResultsScene();
     }
 }
