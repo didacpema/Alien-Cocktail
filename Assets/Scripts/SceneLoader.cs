fda71891ccdf6b45b69649cc3431f7f2ca110eab
Carla Puga <84739899+cpugaper@users.noreply.github.com>
2025-05-19 21:35:39 +0200

    story scene (new game)

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,6 +5,8 @@ public class SceneLoader : MonoBehaviour
 {
     public static SceneLoader Instance { get; private set; }
 
+    private const string STORY_SHOWN_KEY = "StoryShown";
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -22,11 +24,28 @@ public class SceneLoader : MonoBehaviour
 
     }
 
-    public void LoadGameScene()
+    public void LoadGameScene(bool forceStory = false)
+    {
+        if (!forceStory && PlayerPrefs.GetInt(STORY_SHOWN_KEY, 0) == 1)
+        {
+            LoadGameSceneDirectly();
+        }
+        else
+        {
+            LoadGameWithStory();
+        }
+    }
+
+    public void LoadGameWithStory()
+    {
+        PlayerPrefs.SetInt(STORY_SHOWN_KEY, 1);
+        SceneManager.LoadScene("StoryScene");
+    }
+
+    public void LoadGameSceneDirectly()
     {
         SceneManager.LoadScene("GameScene");
         GameManager.Instance?.StartNewShift();
-        
     }
 
     public void LoadResultsScene()
---
+++ Assets/Scripts/StorySceneManager.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+using UnityEngine.XR;
+using UnityEngine.SceneManagement;
+
+public class StorySceneManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI storyText;
+    [SerializeField] private float timePerPhrase = 3f;
+    [SerializeField] private Canvas storyCanvas;
+
+    [TextArea(3, 10)]
+    [SerializeField] private string[] storyPhrases;
+
+    private void Start()
+    {
+        StartCoroutine(InitializeVRCanvas());
+    }
+
+    private IEnumerator InitializeVRCanvas()
+    {
+        yield return new WaitUntil(() => XRSettings.isDeviceActive || XRSettings.loadedDeviceName != "None");
+
+        ConfigureCanvasForVR();
+        StartCoroutine(ShowStory());
+    }
+
+    private void ConfigureCanvasForVR()
+    {
+        if (XRSettings.isDeviceActive)
+        {
+            storyCanvas.renderMode = RenderMode.WorldSpace;
+            storyCanvas.worldCamera = Camera.main;
+
+            CanvasFaceCamera();
+        }
+        else
+        {
+            storyCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        }
+
+        storyText.text = "";
+    }
+
+    private void CanvasFaceCamera()
+    {
+        if (Camera.main != null)
+        {
+            storyCanvas.transform.rotation = Camera.main.transform.rotation;
+        }
+    }
+
+    private IEnumerator ShowStory()
+    {
+        if (storyPhrases == null || storyPhrases.Length == 0)
+        {
+            Debug.LogError("No hay frases configuradas en StorySceneManager");
+            yield break;
+        }
+
+        foreach (string phrase in storyPhrases)
+        {
+            if (storyText != null)
+            {
+                storyText.text = phrase;
+                yield return new WaitForSeconds(timePerPhrase);
+            }
+            else
+            {
+                Debug.LogError("storyText no está asignado en el inspector");
+                yield break;
+            }
+        }
+
+        SceneLoader.Instance?.LoadGameScene();
+    }
+}
